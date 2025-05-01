7dd10741d7bfd703484205c58df1e363c46f23f5
topmost003 <topmostdabest@gmail.com>
2025-05-01 23:09:55 +0100

    js practice

---
+++ script.js
@@ -0,0 +1,86 @@
+// h1 {   onclick="alert(i love you) "}
+
+
+let age = 22
+
+console.log (age)
+
+const family = ("fadipe quadri abiodun")
+
+console.log (family)
+
+let phone = ("iphone itel nokia samsung")
+
+console.log ( phone)
+
+var boys = ("biola,rasak,bashir,ayo,maliq")
+
+console.log ( boys)
+
+// let got = false
+let got = true
+
+console.log( got)
+
+// let doc = " getting started "
+
+// document.writeln ("doc")
+
+// document.write("Getting started with JavaScript tutorial");
+
+
+// let a = 5; // Variable Declaration
+// document.write(a); // Using variable
+// document.write("<br>");
+// let b = "One";
+// document.write(b);
+
+var num1 = 10
+var num2 = 12
+
+let num3 = num1 + num2
+
+let num4 = num2 * num1
+
+// console.log (num1==num2)
+console.log (num3)
+console.log (num4)
+
+console.log ( num1 ,num2 , num3 ,num4)
+
+
+let user = {  name :"fadipe",
+     school : "sail ",
+     age : 20,
+     class : "cohort5"
+}
+
+console.log (user)
+
+
+let message = "hello nigerians"
+
+console.log ( message)
+
+key = 5
+yam = 8
+
+console.log(key ,yam)
+
+let baba
+baba = "popsy"
+
+console.log (baba)
+
+
+let india
+
+console.log(india) 
+// undefined
+
+// array
+
+let student = [ "name:fadipe", "class:noclass" ,"age : 10 " ]
+
+console.log ( student)
+
