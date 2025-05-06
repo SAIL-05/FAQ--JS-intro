8d5da03d14023473e3070fd0bdf7dd91e22c91f8
topmost003 <topmostdabest@gmail.com>
2025-05-06 12:42:58 +0100

    continuation

--- script.js
+++ script.js
@@ -1,240 +1,345 @@
-// h1 {   onclick="alert(i love you) "}
+// // h1 {   onclick="alert(welcome to JAVA SCRIPT) "}
 
 
-let age = 22
+// let age = 22
 
-console.log (age)
+// console.log (age)
 
-const family = ("fadipe quadri abiodun")
+// const family = ("fadipe quadri abiodun")
 
-console.log (family)
+// console.log (family)
 
-let phone = ("iphone itel nokia samsung")
+// let phone = ("iphone itel nokia samsung")
 
-console.log ( phone)
+// console.log ( phone)
 
-var boys = ("biola,rasak,bashir,ayo,maliq")
+// var boys = ("biola,rasak,bashir,ayo,maliq")
 
-console.log ( boys)
+// console.log ( boys)
 
-// let got = false
-let got = true
+// // let got = false
+// let got = true
 
-console.log( got)
+// console.log( got)
 
-// let doc = " getting started "
+// // let doc = " getting started "
 
-// document.writeln ("doc")
+// // document.writeln ("doc")
 
-// document.write("Getting started with JavaScript tutorial");
-
-
-// let a = 5; // Variable Declaration
-// document.write(a); // Using variable
-// document.write("<br>");
-// let b = "One";
-// document.write(b);
-
-var num1 = 10
-var num2 = 12
-
-let num3 = num1 + num2
-
-let num4 = num2 * num1
-
-// console.log (num1==num2)
-console.log (num3)
-console.log (num4)
-
-console.log ( num1 ,num2 , num3 ,num4)
+// // document.write("Getting started with JavaScript tutorial");
 
 
-let user = {  name :"fadipe",
-     school : "sail ",
-     age : 20,
-     class : "cohort5"
+// // let a = 5; // Variable Declaration
+// // document.write(a); // Using variable
+// // document.write("<br>");
+// // let b = "One";
+// // document.write(b);
+
+// var num1 = 10
+// var num2 = 12
+
+// let num3 = num1 + num2
+
+// let num4 = num2 * num1
+
+// // console.log (num1==num2)
+// console.log (num3)
+// console.log (num4)
+
+// console.log ( num1 ,num2 , num3 ,num4)
+
+
+// let user = {  name :"fadipe",
+//      school : "sail ",
+//      age : 20,
+//      class : "cohort5"
+// }
+
+// console.log (user)
+
+
+// let message = "hello nigerians"
+
+// console.log ( message)
+
+// key = 5
+// yam = 8
+
+// console.log(key ,yam)
+
+// let baba
+// baba = "popsy"
+
+// console.log (baba)
+
+
+// let india
+
+// console.log(india) 
+// // undefined
+
+// // array
+
+// let student = [ "name:fadipe", "class:noclass" ,"age : 10 " ]
+
+// console.log ( student)
+
+
+// // data type
+
+// //1. string
+
+// let myName = "fadipe quadri abiodun"
+
+// console.log (myName)
+
+
+// // 2. number
+// let num = 10 
+// console.log (num)
+
+// // 3. boolean
+// // let go = true
+// let go = false
+// console.log (go)
+
+// // 4. null
+// const surname = null
+// console.log (surname)
+
+// // 5. undefined
+// let sur 
+// console.log (sur)
+
+// let friend = " bayo"
+// friend = undefined
+
+// // .6 BigInt
+// let largeNumber = 1234678900987654321n
+
+// console.log (largeNumber)
+
+
+// // OBJECT {key value format}
+// let name = "goat"
+
+// let animal = { name: name  , 
+//      leg : 4,
+//      food :" grass"
+// }
+
+// console.log(animal)
+
+// // Array
+// const color = [
+//      "brown",
+//      "yellow",
+//      "brown",
+//      "green",
+//      "purple"
+// ]
+// console.log (color)
+
+
+// // // Date
+// // let date = "12-23-22"
+
+// // console.log (date)
+
+// // Checking Data Types
+//  let food = "yam"
+// console.log (typeof food)
+
+// let digit = 100
+
+// console.log (typeof digit)
+
+// let present = true
+
+// console.log(typeof present)
+
+// // OPERATORS
+// // 1. ARithmetic
+// let a = 20
+// let b = 10
+
+// console.log ( a + b)
+// console.log ( a - b)
+// console.log ( a * b)
+// console.log ( a % b)
+// console.log ( a / b)
+
+// console.log ( a++)
+// console.log ( a--)
+
+// console.log (b++)
+// console.log (b--)
+
+
+// // 2.comparism operator
+
+// let c = 20 
+// let d = 10
+
+// // equals to
+// console.log (c==d)
+
+// // not equal
+// console.log (c!=d)
+
+// // strictly equal
+// console.log (c===d)
+
+// // strict inequality
+// console.log (c!==d)
+
+// // greater than or equals to
+// console.log (c >= d)
+
+// // less than or equals to
+// console.log (c <= d)
+
+
+// // greater than
+// console.log (c > d)
+
+// // less than
+// console.log (c < d)
+
+
+// // logical operator
+
+// // let e = false
+// // let f =  true
+// // let g = true
+// // let h = false
+
+// // console.log ()
+
+
+
+// //Ayo's Admission Details
+// const fee = true;
+// const uniform = false;
+// const textbooks = false;
+// const idCard = false;
+
+// const checkOR = fee || !textbooks || uniform || idCard;
+// const checkAND = fee && textbooks && uniform && idCard;
+// const checkAyo = fee && textbooks;
+// const checkAyoAgain = (fee && idCard) || (fee && uniform);
+
+
+
+// console.log (checkOR)
+// console.log (checkAND)
+// console.log(checkAyoAgain);
+// console.log("from idcard", !idCard);
+
+
+// // a string and a number wil join together
+// let i = "300"
+// let j = 200
+// let k = "boy"
+
+// console.log (i + j)
+// console.log (i - j)
+// console.log (i - k)
+// console.log (i * k)
+// console.log (i * j)
+
+
+// conditional operator
+
+// ternary
+
+// let age = 18
+// let drinkbeer = age >= 18 ? "You can drink" : "You cannot drink";
+// console.log(drinkbeer);
+
+
+// let age 
+// age = 18
+// let voting = age < 100 && age >= 18 ? "You can vote" : "You cannot vote";
+// console.log(voting);
+
+// let a = 12 
+// let b = 13
+
+// let c = a > b ? "yes ,a is greater than b" : "no,b is greater than a"
+// console.log (c)
+
+
+// let year = 2000 
+
+// let student = { age : year <= 1995 ? "gen-z" : "milennial",
+//         name: "ayo",
+//         class : "ss3"
+// }
+
+// console.log (student)
+
+
+// if statement
+// let startclass = "5pm"
+
+// if (startclass <= "5pm ") { 
+//      console.log ( "we can start class")
+// }
+
+
+// let group = 10
+// if (group <= 15) {
+// console.log ( "yes,they a a group")
+// }
+
+// if else statement
+let group = 10
+if (group <= 9) {
+console.log ( "yes,they a a group")
+}
+else {
+    console.log ("they are not a group") 
 }
 
-console.log (user)
 
+let startclass = "5pm"
 
-let message = "hello nigerians"
-
-console.log ( message)
-
-key = 5
-yam = 8
-
-console.log(key ,yam)
-
-let baba
-baba = "popsy"
-
-console.log (baba)
-
-
-let india
-
-console.log(india) 
-// undefined
-
-// array
-
-let student = [ "name:fadipe", "class:noclass" ,"age : 10 " ]
-
-console.log ( student)
-
-
-// data type
-
-//1. string
-
-let myName = "fadipe quadri abiodun"
-
-console.log (myName)
-
-
-// 2. number
-let num = 10 
-console.log (num)
-
-// 3. boolean
-// let go = true
-let go = false
-console.log (go)
-
-// 4. null
-const surname = null
-console.log (surname)
-
-// 5. undefined
-let sur 
-console.log (sur)
-
-let friend = " bayo"
-friend = undefined
-
-// .6 BigInt
-let largeNumber = 1234678900987654321n
-
-console.log (largeNumber)
-
-
-// OBJECT {key value format}
-let name = "goat"
-
-let animal = { name: name  , 
-     leg : 4,
-     food :" grass"
+if (startclass <= "4pm ") { 
+     console.log ( "we can start class")
+}
+else {
+     console.log ("we cannot start class" )
 }
 
-console.log(animal)
 
-// Array
-const color = [
-     "brown",
-     "yellow",
-     "brown",
-     "green",
-     "purple"
-]
-console.log (color)
+// let number = 15 
+// if (number %  2 === 0) {
+//      console.log ( "even number")
+// }
+// else {
+//      console.log ("odd number ")
+//      }
 
 
-// // Date
-// let date = "12-23-22"
-
-// console.log (date)
-
-// Checking Data Types
- let food = "yam"
-console.log (typeof food)
-
-let digit = 100
-
-console.log (typeof digit)
-
-let present = true
-
-console.log(typeof present)
-
-// OPERATORS
-// 1. ARithmetic
-let a = 20
-let b = 10
-
-console.log ( a + b)
-console.log ( a - b)
-console.log ( a * b)
-console.log ( a % b)
-console.log ( a / b)
-
-console.log ( a++)
-console.log ( a--)
-
-console.log (b++)
-console.log (b--)
-
-
-// 2.comparism operator
-
-let c = 20 
-let d = 10
-
-// equals to
-console.log (c==d)
-
-// not equal
-console.log (c!=d)
-
-// strictly equal
-console.log (c===d)
-
-// strict inequality
-console.log (c!==d)
-
-// greater than or equals to
-console.log (c >= d)
-
-// less than or equals to
-console.log (c <= d)
-
-
-// greater than
-console.log (c > d)
-
-// less than
-console.log (c < d)
-
-
-// logical operator
-
-// let e = false
-// let f =  true
-// let g = true
-// let h = false
-
-// console.log ()
+let number =  prompt("input a number")
+if (number %  2 === 0) {
+     alert ( "even number")
+}
+else {
+     alert("odd number ") 
+     }
 
 
 
-//Ayo's Admission Details
-const fee = true;
-const uniform = false;
-const textbooks = false;
-const idCard = false;
-
-const checkOR = fee || !textbooks || uniform || idCard;
-const checkAND = fee && textbooks && uniform && idCard;
-const checkAyo = fee && textbooks;
-const checkAyoAgain = (fee && idCard) || (fee && uniform);
 
 
 
-console.log (checkOR)
-console.log (checkAND)
-console.log(checkAyoAgain);
-console.log("from idcard", !idCard);
+
+
+
+
+
 
 
 
