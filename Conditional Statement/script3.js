c9d46cc0123ced9e18622f682dc8c75d20c88b31
topmost003 <topmostdabest@gmail.com>
2025-05-09 15:47:03 +0100

    continuation

---
+++ Conditional Statement/script3.js
@@ -0,0 +1,65 @@
+// if statement
+// let startclass = "3pm"
+// if (startclass >= "2pm") { 
+//      console.log ( "we can start class")
+// }
+
+
+// let group = 10
+// if (group <= 15) {
+// console.log ( "yes,they a a group")
+// }
+
+
+
+
+// if else statement
+// let group = 10
+
+// if (group >= 9) {
+// console.log ( "yes,they are group")
+// }
+// else {
+//     console.log ("they are not  group") 
+// }
+
+
+// let minimum = 40
+
+// if ( minimum < 30 ) { 
+//     console.log ( "pass")
+// }
+// else {
+//     console.log (" fail")
+// }
+
+
+
+
+// let startclass = "5pm"
+
+// if (startclass < "4pm ") { 
+//      console.log ( "we can start class")
+// }
+// else {
+//      console.log ("we cannot start class" )
+// }
+
+
+// let number = 30 
+
+// if (number % 2===0) {
+//      console.log ( "even number")
+// }
+// else {
+//      console.log ("odd number ")
+//      }
+
+
+let number =  prompt("input a number")
+if (number %  2 === 0) {
+     alert ( "even number")
+}
+else {
+     alert("odd number ") 
+     }
