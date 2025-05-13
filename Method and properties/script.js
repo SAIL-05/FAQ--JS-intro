e47672436674801ed9277a3c13661f329401be27
topmost003 <topmostdabest@gmail.com>
2025-05-13 22:31:25 +0100

    array

--- Method and properties/script.js
+++ Method and properties/script.js
@@ -1,62 +1,110 @@
-// Array
+// // Array
 
-let a = [ "apple" , "ball" , "cat" , "dog" ," egg" , "fish"]
-console.log (a)
+// let a = [ "apple" , "ball" , "cat" , "dog" ," egg" , "fish"]
+// console.log (a)
 
-// to get the length
-a.length
-console.log(a.length)
+// // to get the length
+// a.length
+// console.log(a.length)
 
-// to get the index number
-// a[2]
-console.log( a[4] )
+// // to get the index number
+// // a[2]
+// console.log( a[4] )
 
-// to add more values to an array
- a.push ("goat")
- console.log (a)
+// // to add more values to an array
+//  a.push ("goat")
+//  console.log (a)
 
- a.push ( "horse")
- console.log (a)
+//  a.push ( "horse")
+//  console.log (a)
 
-//  to remove from back 
-a.pop ()
-console.log(a)
-
-
-// to remove from front
-a.shift ()
-console.log(a)
-
-// to add from Front 
-a.unshift ( "lion")
-console.log (a)
-
-// to check if the value is in the array
-console.log (a.includes("ayo"))
-console.log (a.includes("cat"))
+// //  to remove from back 
+// a.pop ()
+// console.log(a)
 
 
-// to get the index of a value
+// // to remove from front
+// a.shift ()
+// console.log(a)
 
-console.log (a.indexOf ( "cat"))
-console.log (a.indexOf ( "goat"))
+// // to add from Front 
+// a.unshift ( "lion")
+// console.log (a)
 
-// checkif if the index/value is not in the array
-
-console.log ( a.indexOf ( "ayo"))
-
-
-// to attach strings together in an array(can be replaced with anything to space it)
-console.log (a.join(" "))
-
-
-// slice and splice in array 
-console.log ( a.slice ( 2,5))
-// start and end
-
-console.log (a.splice ( 2))
+// // to check if the value is in the array
+// console.log (a.includes("ayo"))
+// console.log (a.includes("cat"))
 
 
+// // to get the index of a value
 
+// console.log (a.indexOf ( "cat"))
+// console.log (a.indexOf ( "goat"))
+
+// // checkif if the index/value is not in the array
+
+// console.log ( a.indexOf ( "ayo"))
+
+
+// // to attach strings together in an array(can be replaced with anything to space it)
+// console.log (a.join(" "))
+
+
+// // slice and splice in array 
+// console.log ( a.slice ( 2,5))
+// // start and end
+
+// console.log (a.splice ( 2))
+
+
+
+const name = [ 'ayo' , 'saka', 'bisi']
+name.splice (1 ,0, "biola")
+
+let slic = name.slice(1)
+
+
+// let myName = (name.concat(number))
+
+// console.log(myName)
+console.log (name)
+
+console.log (slic)
+console.log (name)
+
+// name [2] = "ola"
+// console.log (name)
+
+// name [0] = "india"
+// console.log(name)
+
+
+const number = [ 1,8,6,7,2,3,4,5]
+
+// number.push( 9)
+// number.pop ()  
+// console.log ( number.sort())
+// console.log ( number.at(3))
+
+// console.log(number)
+// console.log(number.toString())
+
+
+// console.log (number[2])
+// console.log (number.length)
+
+// console.log ( name.sort())
+
+
+// let myName = (name.length)
+// console.log (myName)
+
+// let myNumber = ( number.sort())
+// console.log (myNumber)
+
+// console.log (number.length-2 )
+// console.log ( typeof number)
+
+// console.log ( number instanceof Array)
 
 
