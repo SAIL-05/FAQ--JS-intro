73a14b6b4e86e6245cdc56b6dc10c506e391430a
topmost003 <topmostdabest@gmail.com>
2025-05-16 23:33:24 +0100

    function

--- Functions/script.js
+++ Functions/script.js
@@ -1,55 +1,62 @@
-// function user () {
-//    name =" ade"
-//    console.log(name)
-//    alert(name)
-// }
+// // function user () {
+// //    name =" ade"
+// //    console.log(name)
+// //    alert(name)
+// // }
 
-// user()
-
-
-// const user = function (x,y) {
-//     console.log ( x + y)
-// }
-// user(6,8)
+// // user()
 
 
-// function sum (x,y) {
-//   console.log(x + y)
-// }
-//     sum(2,3)
-//    sum(4,7)
-//    sum(56,89)
+// // const user = function (x,y) {
+// //     console.log ( x + y)
+// // }
+// // user(6,8)
 
 
-//    function addTwoDigits () {
-//     let a = prompt("enter first number")
-//     let b = prompt("enter second number")
-//     // let c = a + b
-//     console.log(a + b)
-//    }
-//    addTwoDigits()
+// // function sum (x,y) {
+// //   console.log(x + y)
+// // }
+// //     sum(2,3)
+// //    sum(4,7)
+// //    sum(56,89)
+
+
+// //    function addTwoDigits () {
+// //     let a = prompt("enter first number")
+// //     let b = prompt("enter second number")
+// //     // let c = a + b
+// //     console.log(a + b)
+// //    }
+// //    addTwoDigits()
      
 
 
-const sum = function addNumbers(x, y) {
-    console.log (x + y)
-};
-let z = sum(2, 3);
+// const sum = function addNumbers(x, y) {
+//     console.log (x + y)
+// };
+// let z = sum(2, 3);
+
+// // function greetuser() {
+// //     let greeting = "Good day,you are welcome"
+// //     alert(greeting)
+// // }
+// // greetuser()
+
 
 // function greetuser() {
-//     let greeting = "Good day,you are welcome"
-//     alert(greeting)
+//     let greeting = prompt ("ENTER YOUR NAME")
+//     alert("GOOOD MORNING" + ", " + greeting)
 // }
 // greetuser()
 
-
-function greetuser() {
-    let greeting = prompt ("ENTER YOUR NAME")
-    alert("GOOOD MORNING" + ", " + greeting)
+function greater () {
+    let Number = prompt("enter a number")
+if (Number > 5) {
+    console.log (`${Number}  "is greater than 5"`)
 }
-greetuser()
+}
 
-
+greater();
 
 
 
