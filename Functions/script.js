6640362445be83053defd16943d0052bd413de2c
topmost003 <topmostdabest@gmail.com>
2025-05-18 22:15:21 +0100

    function

--- Functions/script.js
+++ Functions/script.js
@@ -1,62 +1,91 @@
-// // function user () {
-// //    name =" ade"
-// //    console.log(name)
-// //    alert(name)
-// // }
+// // // function user () {
+// // //    name =" ade"
+// // //    console.log(name)
+// // //    alert(name)
+// // // }
 
-// // user()
-
-
-// // const user = function (x,y) {
-// //     console.log ( x + y)
-// // }
-// // user(6,8)
+// // // user()
 
 
-// // function sum (x,y) {
-// //   console.log(x + y)
-// // }
-// //     sum(2,3)
-// //    sum(4,7)
-// //    sum(56,89)
+// // // const user = function (x,y) {
+// // //     console.log ( x + y)
+// // // }
+// // // user(6,8)
 
 
-// //    function addTwoDigits () {
-// //     let a = prompt("enter first number")
-// //     let b = prompt("enter second number")
-// //     // let c = a + b
-// //     console.log(a + b)
-// //    }
-// //    addTwoDigits()
+// // // function sum (x,y) {
+// // //   console.log(x + y)
+// // // }
+// // //     sum(2,3)
+// // //    sum(4,7)
+// // //    sum(56,89)
+
+
+// // //    function addTwoDigits () {
+// // //     let a = prompt("enter first number")
+// // //     let b = prompt("enter second number")
+// // //     // let c = a + b
+// // //     console.log(a + b)
+// // //    }
+// // //    addTwoDigits()
      
 
 
-// const sum = function addNumbers(x, y) {
-//     console.log (x + y)
-// };
-// let z = sum(2, 3);
+// // const sum = function addNumbers(x, y) {
+// //     console.log (x + y)
+// // };
+// // let z = sum(2, 3);
+
+// // // function greetuser() {
+// // //     let greeting = "Good day,you are welcome"
+// // //     alert(greeting)
+// // // }
+// // // greetuser()
+
 
 // // function greetuser() {
-// //     let greeting = "Good day,you are welcome"
-// //     alert(greeting)
+// //     let greeting = prompt ("ENTER YOUR NAME")
+// //     alert("GOOOD MORNING" + ", " + greeting)
 // // }
 // // greetuser()
 
-
-// function greetuser() {
-//     let greeting = prompt ("ENTER YOUR NAME")
-//     alert("GOOOD MORNING" + ", " + greeting)
+// function greater () {
+//     let Number = prompt("enter a number")
+// if (Number > 5) {
+//     console.log (`${Number} "is greater than 5"`)
 // }
-// greetuser()
+// }
+// greater();
 
-function greater () {
-    let Number = prompt("enter a number")
-if (Number > 5) {
-    console.log (`${Number}  "is greater than 5"`)
+
+
+
+// function user () {
+//     let student = "ayo"
+//     console.log (student)
+// }
+
+// user() 
+
+
+function add (a,b){
+    console.log (a + b)
 }
+
+add (2,3)
+add(23 ,98)
+
+
+const a = function( a,b){
+    console.log ( a * b)
 }
 
-greater();
+a(23,89)
 
 
+let totalScore = function ( a ,b) {
+    let c = a + b
+    console.log (c)
+}
 
+totalScore( 45,67)
