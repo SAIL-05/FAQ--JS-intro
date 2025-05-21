fe45bc9906874db850a6cfc3013873a75b48d13a
topmost003 <topmostdabest@gmail.com>
2025-05-22 00:04:34 +0100

    loops

---
+++ loops/script.js
@@ -0,0 +1,15 @@
+// for ( a = 0; a <= 15; a++){
+//     console.log (a)
+// }
+
+// for ( b= 1; b<= 20; b++){
+// if (b % 2===0){
+//     console.log( b, "this number is an even number")
+// }else { console.log(b, "this number is an odd number")}
+// }
+
+ let scores = [ 10,20,30,40,50,60,70,80,90,100]
+
+ for ( c= 1; c<=10;c++ ){
+    console.log( `score =` ,scores[c])
+ }
