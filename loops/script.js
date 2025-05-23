3cccbbb103099a3d84d12ed8a174958a880e4412
topmost003 <topmostdabest@gmail.com>
2025-05-23 23:15:33 +0100

    loop

--- loops/script.js
+++ loops/script.js
@@ -8,8 +8,15 @@
 // }else { console.log(b, "this number is an odd number")}
 // }
 
+
  let scores = [ 10,20,30,40,50,60,70,80,90,100]
 
  for ( c= 1; c<=10;c++ ){
     console.log( `score =` ,scores[c])
+ }
+ 
+
+
+ for ( c=1; c <= scores.length; c++){
+   console.log(  `score =`  ,scores[c])
  }
