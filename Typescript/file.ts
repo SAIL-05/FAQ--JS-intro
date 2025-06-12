df8f1e62f3b06f3bed0932f3f859b06909590d24
topmost003 <topmostdabest@gmail.com>
2025-06-12 22:28:37 +0100

    ts

--- Typescript/file.ts
+++ Typescript/file.ts
@@ -13,28 +13,62 @@
 
 // interface
 
-interface user {
-  myname:string;
-  age: number,
-  student?:boolean
+// interface user {
+//   myname:string;
+//   age: number,
+//   student?:boolean
 
+// }
+
+// let myuser : user = {
+// myname: "ade",
+// age : 39
+
+
+// }
+
+// interface Rectangle {
+//   height: number,
+//   width: number
+// }
+
+// const rectangle: Rectangle = {
+//   height: 20,
+//   width: 10
+// };
+
+
+
+//typescript union
+let mypassword: string | number = "qrqrqrqr"
+
+//parameters
+// function maxnumber(number1: number,number2:number,number3:number):number{
+// return Math.max(number1,number2,number3)
+// }
+
+// maxnumber();
+
+
+let myclass:any = 45
+
+function minNumber() : number {
+    let num1 = 23
+    let num2 = 23
+    let num3 = 23
+    
+return (Math.min(num1,num2,num3))
 }
 
-let myuser : user = {
-myname: "ade",
-age : 39
 
 
+//optional parameter 
+
+function maxnumber(number1: number,number2:number,number3?:number):number{
+return Math.max(number1,number2)
 }
 
-interface Rectangle {
-  height: number,
-  width: number
-}
 
-const rectangle: Rectangle = {
-  height: 20,
-  width: 10
-};
+
 
 
