1abe9aaed07c0ae2da07a218cc27297b6f1437be
topmost003 <topmostdabest@gmail.com>
2025-06-12 22:09:35 +0100

    ts

--- Typescript/file.ts
+++ Typescript/file.ts
@@ -1,11 +1,40 @@
 // annotation
 
-let myname:string = "ade"
+// let myname:string = "ade"
 
-let digit:number = 12
+// let digit:number = 12
 
 
-// inference
-let user = "ade"
-let mynumber: number = 30
+// // inference
+// let user = "ade"
+// let mynumber: number = 30
+
+
+
+// interface
+
+interface user {
+  myname:string;
+  age: number,
+  student?:boolean
+
+}
+
+let myuser : user = {
+myname: "ade",
+age : 39
+
+
+}
+
+interface Rectangle {
+  height: number,
+  width: number
+}
+
+const rectangle: Rectangle = {
+  height: 20,
+  width: 10
+};
+
 
